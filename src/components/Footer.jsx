0c815271f01d24a52eaa57948fe346f65a166e71
dammar093 <dammarrana093@gmail.com>
2024-05-20 20:20:55 +0545

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='text-center py-2 text-white'>
-      All right {new Date().getFullYear()} reserved Dammar Singh Rana
+      All right {new Date().getFullYear()} reserved by Dammar Singh Rana
     </footer>
   )
 }
